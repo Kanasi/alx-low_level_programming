ac8d35cd10904fa6c8298e6084fa064fe736cd5e
kanasi <crispokanasi@gmail.com>
2023-06-15 14:29:52 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints to the standard error output
+ * Return:1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
