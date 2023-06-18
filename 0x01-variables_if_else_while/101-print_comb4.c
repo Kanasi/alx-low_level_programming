355414568c4e1c9569e9269f97e50b9ef3cbd385
kanasi <crispokanasi@gmail.com>
2023-06-18 10:14:43 +0000

    iteration correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,18 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i <= 55; i++)
 	{
 		for (j = 49; j <= 56; j++)
 		{
-			for (k = 50; k <= 55; k++)
+			for (k = 50; k <= 57; k++)
 			{
 				if (j > i && k > j)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i != 57 || j != 56 || k != 55)
+					if (i != 55 || j != 56 || k != 57)
 					{
 						putchar(',');
 						putchar(' ');
