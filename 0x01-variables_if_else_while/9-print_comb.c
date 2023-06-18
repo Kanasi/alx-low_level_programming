067a7d66b0acf056a9882991a954a1d7637288d9
kanasi <crispokanasi@gmail.com>
2023-06-18 08:43:14 +0000

    possible combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Function prints posible single digits
+ * Return:0 exit program
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
