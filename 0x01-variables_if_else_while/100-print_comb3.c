7dd505167ddec8014ca27689c4daae30548bc342
kanasi <crispokanasi@gmail.com>
2023-06-18 09:53:34 +0000

    iteration error

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,15 +7,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 49; j <= 56; j++)
+		for (j = 49; j <= 57; j++)
 		{
 			if (j > i)
 			{
 				putchar(i);
 				putchar(j);
-				if (i != 57 || j != 56)
+				if (i != 56 || j != 57)
 				{
 					putchar(',');
 					putchar(' ');
