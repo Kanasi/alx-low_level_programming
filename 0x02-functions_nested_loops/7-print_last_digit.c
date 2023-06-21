4dc4d6b7b67bc4f05e17eb10e75bbbe433704c96
kanasi <crispokanasi@gmail.com>
2023-06-21 14:59:39 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,13 @@
 
 /**
  * print_last_digit - prints the last digit of a number
+ * Return: last digit
+ * @i: number to be determined
  */
 
-int print_last_digit(int)
+int print_last_digit(int i)
 {
+	int last = i % 10;
 
+	return (last);
+}
