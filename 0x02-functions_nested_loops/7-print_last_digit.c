215ed1c0ae240f58a8db2a9927d8de356bee7413
kanasi <crispokanasi@gmail.com>
2023-06-21 15:09:50 +0000

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,13 @@
 
 int print_last_digit(int i)
 {
-	int last = i % 10;
+	int last;
 
+	if (i < 0)
+		i = -i;
+	last = i % 10;
+	if (last < 0)
+		last = -last;
+	_putchar(last + '0');
 	return (last);
 }
