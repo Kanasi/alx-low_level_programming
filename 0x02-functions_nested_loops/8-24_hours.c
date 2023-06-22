1a87de4a1fedcd2f647ab42e969031446fc6e11b
kanasi <crispokanasi@gmail.com>
2023-06-22 01:27:25 +0000

    24 hrr while loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,12 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h < 24; h++)
+	h = 0;
+
+	while (h < 24)
 	{
-		for (m = 0; m < 60; m++)
+		m = 0;
+		while (m < 60)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
@@ -17,6 +20,8 @@ void jack_bauer(void)
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
+			m++;
 		}
+		h++;
 	}
 }
