47aa76297f47ce80bad9827ab826e6041ff2c9cb
kanasi <crispokanasi@gmail.com>
2023-06-22 01:18:21 +0000

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * @a: int to be checked
+ * Return: absolute value
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	return (-a);
+}
