0d4afa562516d524eb140016a4d50d2852c82e60
kanasi <crispokanasi@gmail.com>
2023-06-23 19:48:00 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints square shape
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int m, n;
+
+	if (size <= 0)
+	{
+		_putchar('\n')
+	} else
+	{
+		for (m = 0; m <= size; m++)
+		{
+			for (n = 0; n <= size; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
