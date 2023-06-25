03fc2def6e893b76fe012edcfbee0562932751c3
kanasi <crispokanasi@gmail.com>
2023-06-25 18:26:12 +0000

    added if condition

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,11 @@ void print_triangle(int size)
 {
 	int a, b;
 
+	if (size == 0)
+	{
+		_putchar('\n');
+	} else
+	{
 	for (a = 0; a < size; a++)
 	{
 		for (b = 0; a < size - a - 1; b++)
@@ -20,5 +25,6 @@ void print_triangle(int size)
 			_putchar('#');
 		}
 		_putchar('\n');
+	}
 	}
 }
