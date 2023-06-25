247c597b97304e42ba27394a85e713a577e8b7eb
kanasi <crispokanasi@gmail.com>
2023-06-25 18:20:33 +0000

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: height of triangle
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; a < size - a - 1; b++)
+		{
+			_putchar(' ');
+		}
+		for (b = 0; b <= a; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
