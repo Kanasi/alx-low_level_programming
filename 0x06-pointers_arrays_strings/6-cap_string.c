5cdb4c6f5221379177fcd76e0caf6dc29dcb124e
kanasi <crispokanasi@gmail.com>
2023-07-04 21:58:04 +0000

    capitalize all words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,9 +2,33 @@
 
 /**
  * *cap_string - capitalize string
+ * @s: string
  * Return: char
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int i, j;
 
+	i = 0;
+	char t[] = "\t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
