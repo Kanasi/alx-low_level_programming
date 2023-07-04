e08681e900688acb76a7baeab1de6b981d9d68ae
kanasi <crispokanasi@gmail.com>
2023-07-04 10:15:13 +0000

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 0; i < (n / 2); i++)
+	for (i = 0; i < n; i++)
 	{
-		b = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = j;
+		k = a[i];
+		a[i] = a[j];
+		a[j] = k;
+
+		i++;
+		j--;
 	}
 }
