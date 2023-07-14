80c5addf50ec0cfd05eaa7fc4f97865db1521c3c
kanasi <crispokanasi@gmail.com>
2023-07-14 00:49:25 +0000

    allocate mem

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memoryusing amlloc
+ * @b: bytes allocated
+ * Return: a pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
