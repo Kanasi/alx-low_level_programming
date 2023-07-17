a19f00b87099ab9b853c34c59d24ca35150148f2
kanasi <crispokanasi@gmail.com>
2023-07-17 15:27:20 -0700

    debug print

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,21 +17,21 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		} else
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 		if (d->age != 0)
 		{
 			printf("Age: %f\n", d->age);
 		} else
 		{
-			printf("Age: 0");
+			printf("Age: 0\n");
 		}
 		if (d->owner != NULL)
 		{
 			printf("Owner: %s\n", d->owner);
 		} else
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 	}
 }
