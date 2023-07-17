1e7c54a5c6340fba7b7541efaaf0afeaadea3fd0
kanasi <crispokanasi@gmail.com>
2023-07-17 13:36:27 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - has three elements
+ * @name: first member
+ * @owner: second member
+ * @age: third member
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	int age;
+};
+
+#endif
