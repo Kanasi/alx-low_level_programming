c5297998ac28d471fec48c439af0ffd38e078bd2
kanasi <crispokanasi@gmail.com>
2023-07-27 18:20:53 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - frees list_t
+ * @head: head address
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current_node);
+	}
+}
