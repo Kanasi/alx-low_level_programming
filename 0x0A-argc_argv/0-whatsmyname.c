9e1094e9eddf39a63fa430609bdf5640dd948cad
kanasi <crispokanasi@gmail.com>
2024-01-23 01:41:51 -0800

    name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point to print name of the program
+ * @argc: no of command
+ * @argv: array of command line agr
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
