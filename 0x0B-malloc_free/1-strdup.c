289cad0a02751868d9b232c37fcd2c58a87ea7e9
kanasi <crispokanasi@gmail.com>
2024-01-29 21:38:17 +0000

    trial 3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,18 +9,23 @@
 char *_strdup(char *str)
 {
 	char *ptr;
+	int i, r = 0;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
-	ptr = malloc(strlen(str) + 1);
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	ptr = malloc(sizeof(char) * (i + 1));
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	strcpy(ptr, str);
+
+	for (r = 0; str[r]; r++)
+		ptr[r] = str[r];
+
 	return (ptr);
 }
