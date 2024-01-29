aadb94c5f41433ba187d849915eba3b59f5fa28b
kanasi <crispokanasi@gmail.com>
2024-01-29 21:49:42 +0000

    concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat- concantenate 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	i = j = 0;
+
+	while (s1[i] != '\0')
+		i++;
+
+	while (s2[j] != '\0')
+		j++;
+
+	ptr = malloc(sizeof(char) * (i + j + 1));
+
+	if (ptr == NULL)
+		return (Null);
+
+	i = j = 0;
+
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[1];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		i++, j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
