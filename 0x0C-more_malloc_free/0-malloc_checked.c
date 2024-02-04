527d3299de7da31e39d234b468c03fd2f8993672
kanasi <crispokanasi@gmail.com>
2024-02-03 23:11:42 -0800

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function checks if memory has been allocated
+ * @b: unsigned int
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
