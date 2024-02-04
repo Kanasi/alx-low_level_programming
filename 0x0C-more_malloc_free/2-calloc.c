e9e6cb18b3f0f950ff480deb667becdd89be2760
kanasi <crispokanasi@gmail.com>
2024-02-04 00:16:06 -0800

    malloc of array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements
+ * @size: bytes for allocation
+ * Return: allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0; || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(size * nmemb);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(p, 0, size * nmemb);
+
+	return (p);
+}
